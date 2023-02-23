4000d10d73c3695c8b6d7b93b0c23569b15327d5
Douglas Dantas <douglas.f.dantas@outlook.com>
2023-02-23 10:22:21 -0300

    Finalizado

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //values set here act as default values.
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // using #imageLiteral(
+        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
+        
+        diceImageView1.alpha = 0.5
+        diceImageView2.alpha = 0.5
+
     }
-
-
+    
+    @IBAction func roll(_ sender: UIButton) {
+        
+        diceImageView1.alpha = 1
+        diceImageView2.alpha = 1
+        
+        // let is used for constants
+        let dices = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageView1.image = dices.randomElement() //opt 1
+        diceImageView2.image = dices[Int.random(in: 0...5)] //opt 2
+        
+        //-- RANDOM COMMANDS --
+        // Int.random(...) or Double.random(...)
+        
+        //-- RANGE OPERATOR --
+        //"0...5" equivale a [0,5]
+        //"0..<5" equivale a [0,5[
+        
+    }
+    
 }
+
+//        print(#file + " " + String(#line))
 
